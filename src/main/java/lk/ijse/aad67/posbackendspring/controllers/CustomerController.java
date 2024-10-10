f34287be47e58461614dba92eea1c4238035aaf8
DILSHAN <dilshanfonseka76@gmail.com>
2024-10-10 12:50:59 +0530

    Regex add

--- src/main/java/lk/ijse/aad67/posbackendspring/controllers/CustomerController.java
+++ src/main/java/lk/ijse/aad67/posbackendspring/controllers/CustomerController.java
@@ -1,11 +1,16 @@
 package lk.ijse.aad67.posbackendspring.controllers;
 
 
+import lk.ijse.aad67.posbackendspring.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/customers")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
 
 }
---
+++ src/main/java/lk/ijse/aad67/posbackendspring/utill/Regex.java
@@ -0,0 +1,7 @@
+package lk.ijse.aad67.posbackendspring.utill;
+
+public class Regex {
+    public static final String CUSTOMER_ID_REGEX = "C\\d{2}-\\d{3}";
+    public static final String ITEM_ID_REGEX = "I\\d{2}-\\d{3}";
+    public static final String ORDER_ID_REGEX = "OID-\\d{3}";
+}
